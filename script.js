9a2372c1ee3d0039cd3f5eaceceffebcd802d8af
wesolypl <wesolowski.patryk@zhr.pl>
2019-01-10 21:26:38 +0100

    add autofill, auto change in localStorage

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const btn = document.querySelector('button');
+const allInput = document.querySelectorAll('input');
 
-btn.addEventListener('click', () => {
-    console.log('click')
+const autofill = () => {
+    allInput.forEach(input => {
+        input.value = localStorage.getItem(input.name);
+    })
+    document.querySelector('p span').textContent = localStorage.getItem('lastChange')
+}
+
+allInput.forEach(input => {
+    input.addEventListener('change', (e) => {
+        localStorage.setItem(e.target.name, e.target.value);
+        const date = new Date().toUTCString();
+        localStorage.setItem('lastChange', date)
+    });
 })
+autofill()
