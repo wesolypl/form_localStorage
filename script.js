f3770d32162b79c4c2a92f1cced0b56467861ba5
wesolypl <wesolowski.patryk@zhr.pl>
2019-01-11 12:25:06 +0100

    add init function, break mapNip function

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
-const allInput = document.querySelectorAll('input');
-
-const autofill = () => {
-    allInput.forEach(input => {
-        input.value = localStorage.getItem(input.name);
-    })
-    document.querySelector('p span').textContent = localStorage.getItem('lastChange')
-}
-
-const maskNip = () => {
-    const nip = document.querySelector('#nipCompany');
-    nip.addEventListener('keyup', () => {
-        if (nip.value.length === 3 || nip.value.length === 7 || nip.value.length === 10) {
-            nip.value = nip.value + "-"
+const maskNip = (target) => {
+    target.addEventListener('keyup', () => {
+        if (target.value.length === 3 || target.value.length === 7 || target.value.length === 10) {
+            target.value = target.value + "-"
+        } else if (target.value.length === 13) {
+            validNip(target);
         }
     })
 }
 
 const validNip = () => {
-    const nip = document.querySelector('#nipCompany');
     const regexp = /^\d{3}-\d{3}-\d{2}-\d{2}$/;
-    if (nip.value.match(regexp)) {
-        console.log('poprawny')
-        addToLoacalStorage("nipCompany", nip.value)
+    if (target.value.match(regexp)) {
+        addToLoacalStorage("nipCompany", target.value)
     }
 }
 
 const addToLoacalStorage = (target, value) => {
-    console.log(target);
-    console.log(value)
     localStorage.setItem(target, value);
     const date = new Date().toUTCString();
     localStorage.setItem('lastChange', date)
     document.querySelector('p span').textContent = localStorage.getItem('lastChange')
 }
 
-allInput.forEach(input => {
-    input.addEventListener('change', (event) => {
-        const target = event.target.name;
-        const value = event.target.value;
-        if (event.target.name !== 'nipCompany') {
-            addToLoacalStorage(target, value)
-        } else if (event.target.name === "nipCompany") {
-            validNip()
-        }
 
-    });
-})
-autofill()
-maskNip()
+const autofill = (allInput) => {
+    allInput.forEach(input => {
+        input.value = localStorage.getItem(input.name);
+    })
+    document.querySelector('p span').textContent = localStorage.getItem('lastChange')
+}
+
+
+const init = () => {
+    const allInput = document.querySelectorAll('input');
+    autofill(allInput);
+    allInput.forEach(input => {
+        input.addEventListener('input', (event) => {
+            if (event.target.name !== 'nipCompany' && event.target.name !== 'status') {
+                const target = event.target.name;
+                const value = event.target.value;
+                addToLoacalStorage(target, value)
+            } else if (event.target.name === "nipCompany") {
+                // debugger
+                const target = event.target.name;
+                console.log(target)
+                maskNip(target);
+
+            } else if (event.target.name === "status") {
+                const target = event.target.name;
+                const value = event.target.id;
+                addToLoacalStorage(target, value)
+            }
+        });
+    })
+}
+
+
+init();
+// maskNip()
+
+// const status = document.querySelectorAll('[name=status]');
+// status.forEach(s => {
+//     s.addEventListener(('click'), (e) => {
+//         console.log(e.target.id)
+//     })
+// })
